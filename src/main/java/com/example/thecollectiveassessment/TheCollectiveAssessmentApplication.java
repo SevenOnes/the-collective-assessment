da6f0ae1a0ff501b4e8eb24a7d405f90d9a28e42
Mustafa Motani <mustafamotani@gmail.com>
2022-07-15 13:59:35 +0500

    Initial commit

---
+++ src/main/java/com/example/thecollectiveassessment/TheCollectiveAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.thecollectiveassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheCollectiveAssessmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheCollectiveAssessmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/thecollectiveassessment/TheCollectiveAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thecollectiveassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheCollectiveAssessmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
